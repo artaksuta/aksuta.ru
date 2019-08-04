0b4ab0fb50493088727cb29e119514686740e04d
Alexey Elizarov <felex-ae@ya.ru>
2019-08-05 01:46:04 +0300

    feat: gh-pages, icon, remove gatsby footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,13 +27,19 @@ const Layout = props => {
           <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
               <li className="nav-home" role="menuitem">
-                <Link to={`/`} activeClassName="nav-current">Главная</Link>
+                <Link to={`/`} activeClassName="nav-current">
+                  Главная
+                </Link>
               </li>
               <li className="nav-about" role="menuitem">
-                <Link to={`/about`} activeClassName="nav-current">Обо мне</Link>
+                <Link to={`/about`} activeClassName="nav-current">
+                  Обо мне
+                </Link>
               </li>
               <li className="nav-elements" role="menuitem">
-                <Link to={`/elements`} activeClassName="nav-current">Элементы</Link>
+                <Link to={`/elements`} activeClassName="nav-current">
+                  Элементы
+                </Link>
               </li>
             </ul>
           </nav>
@@ -86,15 +92,7 @@ const Layout = props => {
         </div>
       </main>
       <footer className="site-foot">
-        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link> &mdash;
-        Создано с помощью{" "}
-        <a
-          href="https://gatsbyjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gatsby
-        </a>
+        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link>
       </footer>
     </div>
   )
